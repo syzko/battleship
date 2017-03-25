e10c05d1a8ad6c813fe57deaec0214f608510b19
syzko <bensingersteven@outlook.com>
2017-03-25 14:00:41 -0400

    Create Carrier.cs

---
+++ Carrier.cs
@@ -0,0 +1,56 @@
+/*////////////////////////////////////////////
+// Programmer: Steven Bensinger			    //
+// Course: GSD311				            //
+// Program Name: Seminar 4 Assignment 2     //
+// Created: 03/13/2017				        //
+// Last Modified: 03/15/2017			    //
+////////////////////////////////////////////*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StevenBensinger_Seminar4_ProgrammingAssignment2
+{
+    class Carrier : Ship
+    {
+        //readonly variables unique to this ship
+        private readonly int length = 5;
+        private readonly ConsoleColor front = ConsoleColor.Black;
+        private readonly ConsoleColor back = ConsoleColor.Cyan;
+        private readonly string aSymbol = " A ";
+
+        //constructor that sets general ship properties to this specific ship
+        public Carrier()
+        {
+            shipLength = length;
+            frontColor = front;
+            backColor = back;
+            Sunk = false;
+            isBattleShip = false;
+            symbol = aSymbol;
+            positionList = new List<Position>();
+
+        }
+
+        //method to reset position and status
+        public void Reset()
+        {
+            Sunk = false;
+            position = new Position();
+            direction = new ShipDirection();
+            positionList.Clear();
+        }
+
+
+        //method to take in coordinates coordinates for positionList
+        public void insertPosition(Position position)
+        {
+            positionList.Add(position);
+
+        }
+
+    }
+}
